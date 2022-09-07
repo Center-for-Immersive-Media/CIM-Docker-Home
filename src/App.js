fe5fd7547c511988b256795686f452db91c7ffc3
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-09-07 10:05:01 -0400

    Content updates

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Center for Immersive Media Internal Resources</h1>
+        <p>An internal listing of resources</p>
+        <div className='lists'>
+        <ul>
+          <h3>Services</h3>
+          <li><a href='http://j125-cim-w04:1000/wp-login.php' target={"_blank"}>Wordpress  (1000)</a></li>
+          <li><a href='http://j125-cim-w04:2000/' target={"_blank"}>Appwrite  (2000)</a></li>
+        </ul>
+        <ul>
+          <h3>AR Demos</h3>
+          <li><a href='8th.io/nrtz6' target={"_blank"}>MoCap Dancer (External)</a></li>
+          <li><a href='8th.io/7a4xw' target={"_blank"}>Postcard (External)</a></li>
+        </ul>
+        </div>
+      </div>
     </div>
   );
 }
