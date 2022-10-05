b11a4c5bd72a12765fab2218daa0a9499f8b5ab9
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-10-05 09:57:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,17 +7,33 @@ function App() {
       <div>
         <h1>Center for Immersive Media Internal Resources</h1>
         <p>An internal listing of resources</p>
-        <div className='lists'>
-        <ul>
-          <h3>Services</h3>
-          <li><a href='http://j125-cim-w04:1000/wp-login.php' target={"_blank"}>Wordpress  (1000)</a></li>
-          <li><a href='http://j125-cim-w04:2000/' target={"_blank"}>Appwrite  (2000)</a></li>
-        </ul>
-        <ul>
-          <h3>AR Demos</h3>
-          <li><a href='8th.io/nrtz6' target={"_blank"}>MoCap Dancer (External)</a></li>
-          <li><a href='8th.io/7a4xw' target={"_blank"}>Postcard (External)</a></li>
-        </ul>
+        <div className="lists">
+          <ul>
+            <h3>Services</h3>
+            <li>
+              <a href="http://j125-cim-w04:8000/wp-login.php" target={"_blank"}>
+                Wordpress (Port:8000)
+              </a>
+            </li>
+            <li>
+              <a href="http://j125-cim-w04:2000/" target={"_blank"}>
+                Appwrite (Port:2000)
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <h3>AR Demos</h3>
+            <li>
+              <a href="8th.io/nrtz6" target={"_blank"}>
+                MoCap Dancer (External)
+              </a>
+            </li>
+            <li>
+              <a href="8th.io/7a4xw" target={"_blank"}>
+                Postcard (External)
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
